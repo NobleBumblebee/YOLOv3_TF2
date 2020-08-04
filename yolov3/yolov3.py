305ef149ea82c4424bdf972b626750844d2257a6
Shamil GEMUEV <https://github.maf-roda.com/>
2020-08-04 09:17:32 +0200

    Yolov3 tf.constant bug fix

--- yolov3/yolov3.py
+++ yolov3/yolov3.py
@@ -15,6 +15,8 @@ from tensorflow.keras.regularizers import l2
 from yolov3.utils import read_class_names
 from yolov3.configs import *
 
+tf.executing_eagerly()
+
 STRIDES         = np.array(YOLO_STRIDES)
 ANCHORS         = (np.array(YOLO_ANCHORS).T/STRIDES).T
 
@@ -23,9 +25,9 @@ class BatchNormalization(BatchNormalization):
     # `layer.trainable = False` is to freeze the layer, so the layer will use
     # stored moving `var` and `mean` in the "inference mode", and both `gama`
     # and `beta` will not be updated !
-    def call(self, x, training=False):
-        if not training:
-            training = tf.constant(False)
+    def call(self, x, training=tf.constant(False)):
+        #if training == False:
+        #    training = tf.constant(False)
         training = tf.logical_and(training, self.trainable)
         return super().call(x, training)
 
